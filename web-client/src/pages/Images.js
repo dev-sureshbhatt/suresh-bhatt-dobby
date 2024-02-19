2063ef6c678920dc25c413fef3805b4d935f14d7
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-02-19 16:46:29 +0530

    fixed minor bugs

--- web-client/src/pages/Images.js
+++ web-client/src/pages/Images.js
@@ -1,31 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import ImageComponent from '../components/ImageComponent'
+import '../App.css'
 
 function Images() {
 
-    const [images, setImages] = useState([{}])
+    const [images, setImages] = useState([])
     const [search, setSearch] = useState('')
 
 
     //for fetching all images when the page component mounts 
-    useEffect(()=>{
+    useEffect(() => {
 
 
         fetch('http://localhost:4000/images', {
             credentials: 'include' //for sending token to verify, authenticate and display get image belonging to the user
-        }).then((response=>{
+        }).then((response => {
 
             if (response.status == 403) {
                 alert("You seems to be not authorized, please register/login")
             }
 
-        response.json().then((response) => {
-            console.log(response)
-            setImages(response)
-        }).catch(err => console.log(err))
+            response.json().then((response) => {
+                console.log(response)
+                setImages(response)
+            }).catch(err => console.log(err))
 
         }))
-        .catch(err => console.log(err))
+            .catch(err => console.log(err))
     }, [])
 
 
@@ -33,21 +34,18 @@ function Images() {
 
     console.log(images)
 
-  return (
-    <div>
-<input value={search} onChange={(ev)=>setSearch(ev.target.value)} placeholder='Search by Image Title...'></input>
+    return (
+        <div className='images-page-wrapper'>
+            <input value={search} onChange={(ev) => setSearch(ev.target.value)} placeholder='Search by Image Title...'></input>
 
-{
-    images
-    .filter(image => image.title.toLowerCase().includes(search))
-    .map(image=> 
-        <ImageComponent key={image._id} {...image}/>
+            {
+                Array.isArray(images) && 
+                images.filter(image => image.title && image.title.toLowerCase().includes(search)).map(image =><ImageComponent key={image._id} {...image} />)
+            }
+
+
+        </div>
     )
-}
-
-        
-    </div>
-  )
 }
 
 export default Images
