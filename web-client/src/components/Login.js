7c5c7b7a992b9f5f6be529b7b92d9ae5b88820f4
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-02-19 19:04:20 +0530

    redirection to all images page after successful login

--- web-client/src/components/Login.js
+++ web-client/src/components/Login.js
@@ -1,43 +1,50 @@
 import React, { useContext, useState } from 'react'
 import './Login.css'
 import UserContext from '../context/UserContext'
+import { Navigate } from 'react-router-dom'
 
 function Login() {
 
-    const [email,setEmail] = useState('')
-const [password, setPassword] = useState('')
-const {validUserInfo, setValidUserInfo, validEmail, setValidEmail} = useContext(UserContext)
+    const [redirect, setRedirect] = useState(false)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const { validUserInfo, setValidUserInfo, validEmail, setValidEmail } = useContext(UserContext)
 
-function handleSubmit(ev){
-    ev.preventDefault()
-    
-    fetch('http://localhost:4000/login', {
-        method: 'POST',
-        body: JSON.stringify({email, password}),
-        credentials: 'include',
-        headers: {'Content-Type':'application/json'}
-    }).then((response)=>{
-        response.json().then((userInfoDoc)=>{alert(userInfoDoc.msg); setValidUserInfo(userInfoDoc.userInfo); setValidEmail(userInfoDoc.userInfo.email)}).catch(err => alert("something went wrong"))
-        
-    }).catch((error)=>{
-        alert("something went wrong")
-    })
+    function handleSubmit(ev) {
+        ev.preventDefault()
+
+        fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json' }
+        }).then((response) => {
+            response.json().then((userInfoDoc) => { alert(userInfoDoc.msg); setValidUserInfo(userInfoDoc.userInfo); setValidEmail(userInfoDoc.userInfo.email); setRedirect(true) }).catch(err => alert("something went wrong"))
+
+        }).catch((error) => {
+            alert("something went wrong")
+        })
 
 
 
 
-}
+    }
 
-  return (
 
-    <div className='container'>
-        <form className='form' onSubmit={handleSubmit}>
-            <input type='email' placeholder='email' value={email} onChange={(ev)=>setEmail(ev.target.value)} />
-            <input type='password' placeholder='password' value={password} onChange={(ev)=>setPassword(ev.target.value)} />
-            <button>Login</button>
-        </form>
-    </div>
-  )
+    if (redirect){
+        return <Navigate to={'/images'} />
+    }
+
+    return (
+
+        <div className='container'>
+            <form className='form' onSubmit={handleSubmit}>
+                <input type='email' placeholder='email' value={email} onChange={(ev) => setEmail(ev.target.value)} />
+                <input type='password' placeholder='password' value={password} onChange={(ev) => setPassword(ev.target.value)} />
+                <button>Login</button>
+            </form>
+        </div>
+    )
 }
 
 export default Login
