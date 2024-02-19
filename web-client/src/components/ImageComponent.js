6cc03d92fcd0482fa14772328eb0a7e0b6580c08
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-02-19 20:01:19 +0530

    conditional rendering and other logics done on UI, editing article feature enabled on UI, now need to handle PATCH request at API to edit image title

--- web-client/src/components/ImageComponent.js
+++ web-client/src/components/ImageComponent.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import './ImageComponent.css'
+import { useState } from 'react'
 
 export default function ImageComponent({...image}) {
 
+
+  const [editTitle, setEditTitle] = useState(false)
+  const [title, setTitle] = useState(image.title)
+
+function handleEdit(){
+  console.log("I have to handle edits now")
+}
+
+
+
   return (
-    <div>
-        <h1>{image.title}</h1>
+    <div className='image-component-container'>
+        <h1>{title}</h1>
+        
+        {editTitle && <input value={title} onChange={(ev)=>{setTitle(ev.target.value)}}></input>}
+        {editTitle ? (<button onClick={handleEdit}>Save</button>) : (<button onClick={ev => setEditTitle(true)}>Edit Title</button>) }
         <img height="500px" width={"600px"} src={`http://localhost:4000/${image.path}.${image.extension}`} /> 
     </div>
   )
